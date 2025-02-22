f431106071d06889960fa261ac8341a6f171494a
Charles McLeod <charlesmcleod@usf.edu>
2025-02-21 20:25:23 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -18,3 +18,22 @@ class Employee {
 const emp1 = new Employee("John Allen", 123, "Sales", 7500);
 console.log(emp1.getDetails()); // "Employee: John Allen, ID: 123, Department: Sales, Salary: $7500"
 console.log(emp1.calculateAnnualSalary());
+
+// Task 2: Creating a Manager Class
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary);
+        this.teamSize = teamSize;
+    }
+    getDetails() {
+        return 'Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}';
+    }
+    calculateBonus() {
+        return this.calculateAnnualSalary() * 0.1; // 10% of annual salary
+    }
+}
+
+// Test case
+const mgr1 = new Manager("Will Johnson", 231, "IT", 10000, 5);
+console.log(mgr1.getDetails());
+console.log(mgr1.calculateBonus());
